ce602b99001dfd52cf02987f0ac8336091571c55
NJOGU <157185184+Nj4191@users.noreply.github.com>
2025-01-24 12:04:45 +0300

    Update main.js
    learned arrays object

--- main.js
+++ main.js
@@ -33,9 +33,64 @@ console.log(d.split(' ,'));
 //Arrays - variables that hold multiple values
 const Numbers = new Array(1,2,3,4,5,6,7,8,9,10,11);
 console.log(Numbers);
+length = Numbers.length;
+ console.log(length);
+ Numbers.push("12");
+ console.log(Numbers);
+ let number = Numbers[Numbers.length - 1];
 const Fruits = new Array("apple", "orange","Banana");
+length = Fruits.length;
+console.log(Fruits);
+let Fruit  = Fruits[Fruits.length - 1];
+let fruit =Fruits[0];
+Fruits.push("mango");
+console.log('Fruits');
 Fruits.forEach(item=>console.log(item));
 Fruits[3]="grapes";
 console.log(Fruits);
 Fruits.push("mango");
 Fruits.unshift("apple");
+//arrays
+let car1 = "BMw";
+let car2 = "AUDI";
+let car3 = "Toyota";
+ let Car4 ="Range rover";
+ //const cars ={"BMW,AUDI,TOYOTA,RANGE ROVER"};
+console.log(car1,car2,car3,Car4);
+ //objects - variables that hold key-value pair
+ let name1 = "james";
+ let name2 = "john";
+ const Person = {
+    firstName : 'james',
+    lastName : 'Njogu',
+    age : 23,
+    hobbies : ["coding","reading"],
+    address: {
+        street: '902 Main Street',
+        city : "Kikuyu",
+        country : "Kenya",
+    }
+ }
+console.log(Person.firstName,Person.age,Person.hobbies[1]);
+const todos = [
+    {
+        id: 1,
+        text: "Buy groceries",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Coding time",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "meet with the boss",
+        completed: false
+    }
+];
+console.log(todos);
+const todosJson = JSON.stringify(todos);
+console.log(todosJson);
+//for loop
+
